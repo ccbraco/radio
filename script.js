4f1a37737337123ca5545a194dd9461e5020a805
faustine <louis.faustine@gmail.com>
2019-05-23 06:38:37 +0200

    stop radio

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // on déclare les variables pour se localiser dans le DOM
 let $flux=  document.getElementById("flux");
 let $btnPlayer= document.getElementById("play");
-
+let $btnStop=document.getElementById("stop");
 
 function PlayRadio() {
     $flux.play();
 } ;
-
+function StopRadio(){
+    $flux.stop();
+}
 
 $btnPlayer.addEventListener('onclick', PlayRadio());
+$btnStop.addEventListener('onclick', stopRadio());
+
 
 
